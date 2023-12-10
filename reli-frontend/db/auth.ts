51373e85dd9485324c957dd6048adea42baea983
ngacho <brandonngacho@gmail.com>
2023-12-10 13:55:11 -0500

    auth to env variable

---
+++ reli-frontend/db/auth.ts
@@ -0,0 +1,11 @@
+import { PoolConfig } from 'pg';
+require('dotenv').config()
+
+// Database configuration
+export const auth: PoolConfig = {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined
+};
