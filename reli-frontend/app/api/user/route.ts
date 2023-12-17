9a4b68b0c0ab8a67043de442eb290d0facb08343
ahmed-k-aly <ahmedkaly1101@gmail.com>
2023-12-17 12:38:46 -0500

    deleted unnecessary routes for user

--- reli-frontend/app/api/user/route.ts
+++ reli-frontend/app/api/user/route.ts
@@ -3,97 +3,27 @@ import { getClient, query } from 'db/db';
 import { NextRequest, NextResponse } from 'next/server';
 
 
-export async function GET(req: NextRequest) {
+import { z } from 'zod';
 
-    const searchParams = req.nextUrl.searchParams;
-    const queryParams = [];
-    const whereClauses = [];
-    
-    for (const [key, value] of searchParams) {
-        if (value) {
-            queryParams.push(value);
-            whereClauses.push(`${key} = $${queryParams.length}`);
-        }
-    }
-    if (whereClauses.length === 0) {
-        return new Response("GET request received but no query parameters provided", { status: 400 });
-    }
-
-    let queryString = 'SELECT * FROM "User"';
-    if (whereClauses.length > 0) {
-        queryString += ` WHERE ${whereClauses.join(' AND ')}`;
-    }
-
-    const client = await getClient();
-    try {
-        const result = await client.query(queryString, queryParams);
-        if (result.rows.length === 0) {
-            return new NextResponse('User not found', { status: 404 });
-        } else {
-            return NextResponse.json(result.rows);
-        }
-    } catch (error) {
-        return new NextResponse(String(error), { status: 500 });
-    } finally {
-        client.release();
-    }
-};
-
-
-export async function POST (req: NextRequest) {
-    const client = await getClient();
-    try {
-        const searchParams = req.nextUrl.searchParams; 
-        const email = searchParams.get('email');
-        const role = searchParams.get('role');
-        if (!email || !role) {
-            return new NextResponse('Email and role are required', { status: 400 });
-        }
-        const result = await client.query('INSERT INTO "User" (email, role) VALUES ($1, $2) ON CONFLICT (email) DO NOTHING RETURNING * ', [email, role]);
-        return NextResponse.json(result.rows[0]);
-    } catch (error) {
-        return new NextResponse('Internal Server Error', { status: 500 });
-    } finally {
-        client.release();
-    }
-};
-
-export async function PUT (req: NextRequest) {
-    const client = await getClient();
-    try {
-        
-        const searchParams = req.nextUrl.searchParams; 
-        const email = searchParams.get('email');
-        const role = searchParams.get('role');
-
-        if (!email || !role) {
-            return new NextResponse('Email and role are required', { status: 400 });
-        }
-
-        const result = await client.query('UPDATE "User" SET role = $1 WHERE email = $2 RETURNING *', [role, email]);
-        if (result.rows.length === 0) {
-            return new NextResponse('User not found', { status: 404 });
-        } else {
-            return NextResponse.json(result.rows[0]);
-        }
-    } catch (error) {
-        return new NextResponse('Internal Server Error', { status: 500 });
-    } finally {
-        client.release();
-    }
-};
+// Define the input validation schema using Zod
+const loginInputSchema = z.object({
+    email: z.string().email(),
+});
 
 export async function DELETE (req: NextRequest) {
     const client = await getClient();
+
+
     try {
-        const searchParams = req.nextUrl.searchParams; 
-        const email = searchParams.get('email');
+        const input  = await req.json();
+        const validatedInput = loginInputSchema.parse(input);
+        const email = validatedInput.email;
         const result = await client.query('DELETE FROM "User" WHERE email = $1 RETURNING *', [email]);
 
         if (result.rows.length === 0) {
             return new NextResponse('User not found', { status: 404 });
         } else {
-            return NextResponse.json(result.rows[0]);
+            return NextResponse.json({ success: true, message: 'User deleted'  }, {status: 200});
         }
     } catch (error) {
         return new NextResponse('Internal Server Error', { status: 500 });
